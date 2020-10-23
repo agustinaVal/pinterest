daf17085dd7c985f1fb6bfd7058d67a533c87d39
Agustina <maria.agustina.valenti89@gmail.com>
2020-10-22 22:58:22 -0300

    subiendo layout nav-bar

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-container>
+	<v-container fluid px-16>
 		<v-row class="mb-6" pl-3>
 			<v-col cols="6" md="3" v-for="n in 4" :key="n">
 				<template>
@@ -7,6 +7,7 @@
 						<template v-slot:default="{ hover }">
 							<v-card class="mx-auto" max-width="344">
 								<v-img src="https://cdn.vuetifyjs.com/images/cards/forest-art.jpg"></v-img>
+
 								<v-fade-transition>
 									<v-overlay v-if="hover" absolute color="#036358">
 										<v-btn>See more info</v-btn>
