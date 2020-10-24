4451f5be7e6cf0cc8d9fad1c5d19aa9f2b6b28be
Agustina <maria.agustina.valenti89@gmail.com>
2020-10-24 17:29:11 -0300

    layaout vista principal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<v-app-bar color="deep-purple accent-4" dense dark>
+	<v-app>
+		<v-app-bar app color="white" elevate-on-scroll>
 			<v-app-bar-nav-icon></v-app-bar-nav-icon>
 
 			<v-toolbar-title>Page title</v-toolbar-title>
@@ -29,17 +29,30 @@
 				</v-list>
 			</v-menu>
 		</v-app-bar>
-    <v-main>
-		<router-view></router-view>
-	</v-main>
-	</div>
+
+		<!-- Sizes your content based upon application components -->
+		<v-main>
+			<!-- Provides the application the proper gutter -->
+			<v-container fluid>
+				<Grid />
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+
+		<v-footer app>
+			<!-- -->
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
+import Grid from "./components/Grid.vue"
 export default {
 	name: 'App',
 
-	components: {},
+	components: {
+    Grid
+  },
 
 	data: () => ({
 		//
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-banner>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+    Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+    <template v-slot:actions>
+      <v-btn
+        text
+        color="primary"
+      >
+        Dismiss
+      </v-btn>
+      <v-btn
+        text
+        color="primary"
+      >
+        Retry
+      </v-btn>
+    </template>
+  </v-banner>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
